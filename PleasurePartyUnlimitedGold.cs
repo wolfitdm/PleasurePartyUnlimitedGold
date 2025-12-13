b131fc4e545c09898b98e0d6989b34b25ce99f0f
wolfitdm <wolfitdm@gmail.com>
2025-12-13 11:59:30 +0100

    Pleasure Party Unlimited Gold

---
+++ PleasurePartyUnlimitedGold.cs
@@ -0,0 +1,37 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using BepInEx.Unity.Mono;
+using HarmonyLib;
+using System;
+
+namespace PleasurePartyUnlimitedGold
+{
+    [BepInPlugin("com.wolfitdm.PleasurePartyUnlimitedGold", "PleasurePartyUnlimitedGold Plugin", "1.0.0.0")]
+    public class PleasurePartyUnlimitedGold : BaseUnityPlugin
+    {
+        internal static new ManualLogSource Logger;
+        public PleasurePartyUnlimitedGold()
+        {
+        }
+
+        public static Type MyGetType(string originalClassName)
+        {
+            return Type.GetType(originalClassName + ",Assembly-CSharp");
+        }
+        private void Awake()
+        {
+            // Plugin startup logic
+            Logger = base.Logger;
+            Harmony.CreateAndPatchAll(typeof(PleasurePartyUnlimitedGold));
+            Logger.LogInfo($"Plugin PleasurePartyUnlimitedGold is loaded!");
+        }
+
+        [HarmonyPatch(typeof(Assets.FantasyInventory.Scripts.Interface.Shop), "loadInventory")] // Specify target method with HarmonyPatch attribute
+        [HarmonyPostfix]                              // There are different patch types. Prefix code runs before original code
+        static void loadIventory(object __instance)
+        {
+            Assets.FantasyInventory.Scripts.Interface.Shop _this = (Assets.FantasyInventory.Scripts.Interface.Shop)__instance;
+            _this.inventory.Add(new Assets.FantasyInventory.Scripts.Data.Item(Assets.FantasyInventory.Scripts.Enums.ItemId.Gold, 100000));
+        }
+    }
+}
--- obj/Release/net40/MyPluginInfo.cs
+++ obj/Release/net40/MyPluginInfo.cs
@@ -1,9 +1,9 @@
-namespace AlwaysBeNaked
+namespace PleasurePartyUnlimitedGold
 {
     public static class MyPluginInfo
     {
-        public const string PLUGIN_GUID = "AlwaysBeNaked";
-        public const string PLUGIN_NAME = "You can overall be naked!";
+        public const string PLUGIN_GUID = "PleasurePartyUnlimitedGold";
+        public const string PLUGIN_NAME = "You have unlimited gold!";
         public const string PLUGIN_VERSION = "1.0.0";
     }
 }
